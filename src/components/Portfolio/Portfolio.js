a386e8b9e1aa36f4c3b8d1f01da6775cf2205851
Dylan Best <best.dylan15@gmail.com>
2020-11-08 21:55:01 -0500

    deployment

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PortfolioItem from "./PortfolioItem";
 import "./portfolio.css";
-import image1 from "./assets/portfolioitem1.png";
 import AOS from "aos";
 
 function Portfolio() {
@@ -18,7 +17,7 @@ function Portfolio() {
     {
       id: 1,
       name: "Work Bytes",
-      image: image1,
+      image: "https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png",
       tech: "jQuery / Materialize",
       link: "https://relizabet.github.io/Work_Bytes/",
       github: "https://github.com/dylanbest15/Work-Bytes",
@@ -27,7 +26,7 @@ function Portfolio() {
       id: 2,
       name: "Outdoor Adventure Buddies",
       image: "https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png",
-      tech: "Express-Handlebars / MySQL",
+      tech: "Handlebars / MySQL",
       link: "https://whispering-castle-57159.herokuapp.com/",
       github: "https://github.com/dylanbest15/Outdoor-Adventure-Buddies"
     },
@@ -43,7 +42,7 @@ function Portfolio() {
       id: 4,
       name: "Fitness Tracker",
       image: "https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png",
-      tech: "Express.js / MongoDB",
+      tech: "Express / MongoDB",
       link: "https://warm-spire-59876.herokuapp.com/",
       github: "https://github.com/dylanbest15/Fitness-Tracker"
     }
--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function PortfolioItem(props) {
   return (
-    <div className="col-12 col-md-3">
+    <div className="col-12 col-md-6">
       <div className="container item-container">
         <img src={props.image} alt={props.name}></img>
         <div className="middle">
           <p className="text-name">{props.name}</p>
           <p className="text-tech">{props.tech}</p>
-          <a href={props.link}><button className="btn btn-primary btn-livelink">Live Link</button></a>
-          <a href={props.github}><button className="btn btn-primary btn-github">See the Code</button></a>
+          <a href={props.link}><button className="btn btn-primary btn-livelink">Visit website</button></a>
+          <a href={props.github}><button className="btn btn-primary btn-github">See the code</button></a>
         </div>
       </div>
     </div>
