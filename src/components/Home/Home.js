2e759e11d7d49bbc7c7bf7175d9d935da8992f87
Dylan Best <best.dylan15@gmail.com>
2020-11-12 11:06:01 -0500

    minimized some things

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <h1 className="animate__animated animate__fadeIn">Hi, I'm <span className="name">Dylan Best.</span></h1>
         <h2 className="animate__animated animate__fadeIn">I'm a full-stack web developer.</h2>
         <a href="#about" className="animate__animated animate__fadeIn"> 
-          <button className="btn btn-primary btn-lg">Check out my work
+          <button className="btn btn-primary">Check out my work
           <i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i></button></a>
       </div>
 
