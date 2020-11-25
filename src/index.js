17a3a74f68ad6898b7f0a2b61bc0478487525f64
Dylan Best <best.dylan15@gmail.com>
2020-11-24 20:21:01 -0500

    jquery and popper install

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "animate.css/animate.min.css";
 import "aos/dist/aos.css";
 
